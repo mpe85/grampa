38c495b9164fb105401eed7b0d6deb14f4a1c01d
Marco Perazzo <marco.perazzo85@gmail.com>
2018-07-25 00:14:54 +0200

    Added test for test/testNot matchers

--- src/test/java/com/mpe85/grampa/parser/AbstractParserTest.java
+++ src/test/java/com/mpe85/grampa/parser/AbstractParserTest.java
@@ -1,20 +1,22 @@
 package com.mpe85.grampa.parser;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
-import com.mpe85.grampa.matcher.IMatcher;
+import com.mpe85.grampa.matcher.Rule;
 import com.mpe85.grampa.runner.ParseResult;
 import com.mpe85.grampa.runner.ParseRunner;
 
 public class AbstractParserTest {
 	
 	@Test
-	public void test_action() {
+	public void action() {
 		final class Parser extends AbstractParser<Integer> {
 			@Override
-			public IMatcher<Integer> root() {
+			public Rule<Integer> root() {
 				return action(ctx -> ctx.getValueStack().push(4711));
 			}
 		}
@@ -23,10 +25,10 @@ public class AbstractParserTest {
 	}
 	
 	@Test
-	public void test_push() {
+	public void push() {
 		final class Parser extends AbstractParser<Integer> {
 			@Override
-			public IMatcher<Integer> root() {
+			public Rule<Integer> root() {
 				return push(4711);
 			}
 		}
@@ -35,10 +37,10 @@ public class AbstractParserTest {
 	}
 	
 	@Test
-	public void test_sequence_push() {
+	public void sequence_push() {
 		final class Parser extends AbstractParser<Integer> {
 			@Override
-			public IMatcher<Integer> root() {
+			public Rule<Integer> root() {
 				return sequence(
 						push(4711),
 						push(ctx -> peek(ctx) + 4),
@@ -56,6 +58,70 @@ public class AbstractParserTest {
 		assertEquals(2, result.getValueStack().size());
 		assertEquals(Integer.valueOf(9426), result.getValueStack().peek());
 		assertEquals(Integer.valueOf(4715), result.getValueStack().peek(1));
+	}
+	
+	@Test
+	public void test_valid() {
+		final class Parser extends AbstractParser<Integer> {
+			@Override
+			public Rule<Integer> root() {
+				return sequence(
+						test(string("what")),
+						string("whatever"));
+			}
+		}
+		final ParseRunner<Integer> runner = new ParseRunner<>(new Parser());
+		final ParseResult<Integer> result = runner.run("whatever");
+		assertTrue(result.isMatched());
+		assertTrue(result.isMatchedWholeInput());
+	}
+	
+	@Test
+	public void test_invalid() {
+		final class Parser extends AbstractParser<Integer> {
+			@Override
+			public Rule<Integer> root() {
+				return sequence(
+						test(string("ever")),
+						string("whatever"));
+			}
+		}
+		final ParseRunner<Integer> runner = new ParseRunner<>(new Parser());
+		final ParseResult<Integer> result = runner.run("whatever");
+		assertFalse(result.isMatched());
+		assertFalse(result.isMatchedWholeInput());
+	}
+	
+	@Test
+	public void testNot_valid() {
+		final class Parser extends AbstractParser<Integer> {
+			@Override
+			public Rule<Integer> root() {
+				return sequence(
+						testNot(string("foo")),
+						string("whatever"));
+			}
+		}
+		final ParseRunner<Integer> runner = new ParseRunner<>(new Parser());
+		final ParseResult<Integer> result = runner.run("whatever");
+		assertTrue(result.isMatched());
+		assertTrue(result.isMatchedWholeInput());
+	}
+	
+	@Test
+	public void testNot_invalid() {
+		final class Parser extends AbstractParser<Integer> {
+			@Override
+			public Rule<Integer> root() {
+				return sequence(
+						testNot(string("what")),
+						string("whatever"));
+			}
+		}
+		final ParseRunner<Integer> runner = new ParseRunner<>(new Parser());
+		final ParseResult<Integer> result = runner.run("whatever");
+		assertFalse(result.isMatched());
+		assertFalse(result.isMatchedWholeInput());
 	}
 	
 }
