9a05f940ce740cb37fc6467b917a26e0d4744676
Marco Perazzo <marco.perazzo85@gmail.com>
2018-08-16 00:51:54 +0200

    Added spotbugs annotations

--- src/test/java/com/mpe85/grampa/SuperParser.java
+++ src/test/java/com/mpe85/grampa/SuperParser.java
@@ -2,9 +2,12 @@ package com.mpe85.grampa;
 
 import com.mpe85.grampa.rule.Rule;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
 public class SuperParser extends TestParser {
 	
 	@Override
+	@SuppressFBWarnings("IL_INFINITE_RECURSIVE_LOOP")
 	public Rule<String> root() {
 		return sequence(character('a'), this.root());
 	}
