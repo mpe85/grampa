ac210c14a18a110b7da846ccf82fb661f26debe7
Marco Perazzo <marco.perazzo85@gmail.com>
2019-04-04 12:50:59 +0200

    Fixed typo

--- src/main/java/com/mpe85/grampa/parser/AbstractParser.java
+++ src/main/java/com/mpe85/grampa/parser/AbstractParser.java
@@ -972,7 +972,7 @@ public abstract class AbstractParser<T> implements Parser<T> {
 	
 	/**
 	 * Pops and casts the top level element from the stack. This method may be called by an action or command where the
-	 * action. context is available.
+	 * action context is available.
 	 * 
 	 * @param <U>
 	 *            a sub type of T
