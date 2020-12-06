dafefcd30f2934ad9bf5b0baf00961ff54fc6681
Marco Perazzo <marco.perazzo85@gmail.com>
2020-12-06 23:57:53 +0100

    Kotlin conversion

--- src/main/kotlin/com/mpe85/grampa/util/stack/RestorableStack.kt
+++ src/main/kotlin/com/mpe85/grampa/util/stack/RestorableStack.kt
@@ -1,171 +1,149 @@
-package com.mpe85.grampa.util.stack;
+package com.mpe85.grampa.util.stack
 
-import java.util.Deque;
+import java.util.Deque
 
 /**
  * A stack data structure that allows taking and restoring snapshots.
- * 
+ *
  * @author mpe85
  *
- * @param <E>
- *            the type of the stack elements
+ * @param E the type of the stack elements
  */
-public interface RestorableStack<E> extends Deque<E> {
-	
-	/**
-	 * Inserts an element onto the stack at a given position.
-	 * 
-	 * @param down
-	 *            the position where the element is inserted, counted downwards beginning at the top of the stack (i.e.
-	 *            the number of elements on the stack to skip).
-	 * @param element
-	 *            an element
-	 */
-	void push(int down, E element);
-	
-	/**
-	 * Retries and removes an element from the stack at a given position.
-	 * 
-	 * @param down
-	 *            the position where the element is popped, counted downwards beginning at the top of the stack (i.e.
-	 *            the number of elements on the stack to skip).
-	 * @return the popped element
-	 */
-	E pop(int down);
-	
-	/**
-	 * Retries and removes an element from the stack and casts it to the specified sub type.
-	 * 
-	 * @param <T>
-	 *            a sub type of E
-	 * @param type
-	 *            the type to cast the element to
-	 * @return the popped element
-	 */
-	<T extends E> T popAs(Class<T> type);
-	
-	/**
-	 * Retries and removes an element from the stack at a given position and casts it to the specified sub type.
-	 * 
-	 * @param <T>
-	 *            a sub type of E
-	 * @param down
-	 *            the position where the element is popped, counted downwards beginning at the top of the stack (i.e.
-	 *            the number of elements on the stack to skip).
-	 * @param type
-	 *            the type to cast the element to
-	 * @return the popped element
-	 */
-	<T extends E> T popAs(int down, Class<T> type);
-	
-	/**
-	 * Retrieves, but does not remove an element from the stack at a given position.
-	 * 
-	 * @param down
-	 *            the position where the element is peeked, counted downwards beginning at the top of the stack (i.e.
-	 *            the number of elements on the stack to skip).
-	 * @return the peeked element
-	 */
-	E peek(int down);
-	
-	/**
-	 * Retrieves, but does not remove an element from the stack and casts it to the specified sub type.
-	 * 
-	 * @param <T>
-	 *            a sub type of E
-	 * @param type
-	 *            the type to cast the element to
-	 * @return the peeked element
-	 */
-	<T extends E> T peekAs(Class<T> type);
-	
-	/**
-	 * Retrieves, but does not remove an element from the stack at a given position and casts it to the specified sub
-	 * type.
-	 * 
-	 * @param <T>
-	 *            a sub type of E
-	 * @param down
-	 *            the position where the element is peeked, counted downwards beginning at the top of the stack (i.e.
-	 *            the number of elements on the stack to skip).
-	 * @param type
-	 *            the type to cast the element to
-	 * @return the peeked element
-	 */
-	<T extends E> T peekAs(int down, Class<T> type);
-	
-	/**
-	 * Replaces the element on top of the stack.
-	 * 
-	 * @param element
-	 *            a replacement element
-	 * @return the replaced element
-	 */
-	E poke(E element);
-	
-	/**
-	 * Replaces an element in the stack at a given position.
-	 * 
-	 * @param down
-	 *            the position where the element is poked, counted downwards beginning at the top of the stack (i.e. the
-	 *            number of elements on the stack to skip).
-	 * @param element
-	 *            a replacement element
-	 * @return the replaced element
-	 */
-	E poke(int down, E element);
-	
-	/**
-	 * Duplicates the element on top of the stack, i.e. the top stack element gets pushed again.
-	 * 
-	 */
-	void dup();
-	
-	/**
-	 * Swaps the two top stack elements on the stack.
-	 */
-	void swap();
-	
-	/**
-	 * Takes a snapshot of the current stack state. May be called multiple times.
-	 */
-	void takeSnapshot();
-	
-	/**
-	 * Restores the stack state to the last taken snapshot.
-	 */
-	void restoreSnapshot();
-	
-	/**
-	 * Discards the last taken snapshot.
-	 */
-	void discardSnapshot();
-	
-	/**
-	 * Removes the last taken snapshot.
-	 * 
-	 * @param restore
-	 *            true if the stack should be restored to the snapshot, false if the snapshot should be discarded.
-	 */
-	void removeSnapshot(boolean restore);
-	
-	/**
-	 * Removes all taken snapshots.
-	 */
-	void clearAllSnapshots();
-	
-	/**
-	 * Gets the current number of snapshots.
-	 * 
-	 * @return the number of snapshots
-	 */
-	int getSnapshotCount();
-	
-	/**
-	 * Copies the stack. Only the references to the elements are copied, not the elements themselves.
-	 * 
-	 * @return the stack copy
-	 */
-	RestorableStack<E> copy();
-	
+interface RestorableStack<E> : Deque<E> {
+  /**
+   * Inserts an element onto the stack at a given position.
+   *
+   * @param down the position where the element is inserted, counted downwards beginning at the top of the stack
+   *             (i.e. the number of elements on the stack to skip).
+   * @param element an element
+   */
+  fun push(down: Int, element: E)
+
+  /**
+   * Retries and removes an element from the stack at a given position.
+   *
+   * @param down the position where the element is popped, counted downwards beginning at the top of the stack
+   *             (i.e. the number of elements on the stack to skip).
+   * @return the popped element
+   */
+  fun pop(down: Int): E
+
+  /**
+   * Retries and removes an element from the stack and casts it to the specified sub type.
+   *
+   * @param T a sub type of E
+   * @param type the type to cast the element to
+   * @return the popped element
+   */
+  fun <T : E> popAs(type: Class<T>): T
+
+  /**
+   * Retries and removes an element from the stack at a given position and casts it to the specified sub type.
+   *
+   * @param T a sub type of E
+   * @param down the position where the element is popped, counted downwards beginning at the top of the stack
+   *             (i.e. the number of elements on the stack to skip).
+   * @param type the type to cast the element to
+   * @return the popped element
+   */
+  fun <T : E> popAs(down: Int, type: Class<T>): T
+
+  /**
+   * Retrieves, but does not remove an element from the stack at a given position.
+   *
+   * @param down the position where the element is peeked, counted downwards beginning at the top of the stack
+   *             (i.e. the number of elements on the stack to skip).
+   * @return the peeked element
+   */
+  fun peek(down: Int): E
+
+  /**
+   * Retrieves, but does not remove an element from the stack and casts it to the specified sub type.
+   *
+   * @param T a sub type of E
+   * @param type the type to cast the element to
+   * @return the peeked element
+   */
+  fun <T : E> peekAs(type: Class<T>): T
+
+  /**
+   * Retrieves, but does not remove an element from the stack at a given position and casts it to the specified sub
+   * type.
+   *
+   * @param T a sub type of E
+   * @param down the position where the element is peeked, counted downwards beginning at the top of the stack
+   *             (i.e. the number of elements on the stack to skip).
+   * @param type the type to cast the element to
+   * @return the peeked element
+   */
+  fun <T : E> peekAs(down: Int, type: Class<T>): T
+
+  /**
+   * Replaces the element on top of the stack.
+   *
+   * @param element a replacement element
+   * @return the replaced element
+   */
+  fun poke(element: E): E
+
+  /**
+   * Replaces an element in the stack at a given position.
+   *
+   * @param down the position where the element is poked, counted downwards beginning at the top of the stack
+   *             (i.e. the number of elements on the stack to skip).
+   * @param element a replacement element
+   * @return the replaced element
+   */
+  fun poke(down: Int, element: E): E
+
+  /**
+   * Duplicates the element on top of the stack, i.e. the top stack element gets pushed again.
+   */
+  fun dup()
+
+  /**
+   * Swaps the two top stack elements on the stack.
+   */
+  fun swap()
+
+  /**
+   * Takes a snapshot of the current stack state. May be called multiple times.
+   */
+  fun takeSnapshot()
+
+  /**
+   * Restores the stack state to the last taken snapshot.
+   */
+  fun restoreSnapshot()
+
+  /**
+   * Discards the last taken snapshot.
+   */
+  fun discardSnapshot()
+
+  /**
+   * Removes the last taken snapshot.
+   *
+   * @param restore true if the stack should be restored to the snapshot, false if the snapshot should be discarded.
+   */
+  fun removeSnapshot(restore: Boolean)
+
+  /**
+   * Removes all taken snapshots.
+   */
+  fun clearAllSnapshots()
+
+  /**
+   * Gets the current number of snapshots.
+   *
+   * @return the number of snapshots
+   */
+  val snapshotCount: Int
+
+  /**
+   * Copies the stack. Only the references to the elements are copied, not the elements themselves.
+   *
+   * @return the stack copy
+   */
+  fun copy(): RestorableStack<E>
 }
--- src/main/kotlin/com/mpe85/grampa/util/stack/impl/LinkedListRestorableStack.kt
+++ src/main/kotlin/com/mpe85/grampa/util/stack/impl/LinkedListRestorableStack.kt
@@ -1,156 +1,94 @@
-package com.mpe85.grampa.util.stack.impl;
+package com.mpe85.grampa.util.stack.impl
 
-import static com.google.common.base.Preconditions.checkNotNull;
-import static com.google.common.base.Preconditions.checkPositionIndex;
-
-import java.util.ArrayDeque;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Deque;
-import java.util.LinkedList;
-
-import com.mpe85.grampa.util.stack.RestorableStack;
+import com.google.common.base.Preconditions
+import com.mpe85.grampa.util.stack.RestorableStack
+import java.util.ArrayDeque
+import java.util.Collections
+import java.util.Deque
+import java.util.LinkedList
 
 /**
  * A linked list implementation of a restorable stack.
- * 
+ *
  * @author mpe85
  *
- * @param <E>
- *            the type of the stack elements
+ * @param E the type of the stack elements
  */
-public class LinkedListRestorableStack<E> extends LinkedList<E> implements RestorableStack<E> {
-	
-	private static final long serialVersionUID = 3875323652049358971L;
-	
-	private final Deque<Deque<E>> snapshots = new ArrayDeque<>();
-	
-	/**
-	 * Default c'tor.
-	 */
-	public LinkedListRestorableStack() {
-	}
-	
-	/**
-	 * C'tor.
-	 * 
-	 * @param c
-	 *            the initial elements of the stack.
-	 */
-	public LinkedListRestorableStack(final Collection<? extends E> c) {
-		super(c);
-	}
-	
-	@Override
-	public void push(final int down, final E element) {
-		add(checkIndex(down), element);
-	}
-	
-	@Override
-	public E pop(final int down) {
-		return remove(checkIndex(down));
-	}
-	
-	@Override
-	public <T extends E> T popAs(final Class<T> type) {
-		checkNotNull(type, "A 'type' must not be null.");
-		return type.cast(pop());
-	}
-	
-	@Override
-	public <T extends E> T popAs(final int down, final Class<T> type) {
-		checkNotNull(type, "A 'type' must not be null.");
-		return type.cast(pop(checkIndex(down)));
-	}
-	
-	@Override
-	public E peek(final int down) {
-		return get(checkIndex(down));
-	}
-	
-	@Override
-	public <T extends E> T peekAs(final Class<T> type) {
-		checkNotNull(type, "A 'type' must not be null.");
-		return type.cast(get(0));
-	}
-	
-	@Override
-	public <T extends E> T peekAs(final int down, final Class<T> type) {
-		checkNotNull(type, "A 'type' must not be null.");
-		return type.cast(get(checkIndex(down)));
-	}
-	
-	@Override
-	public E poke(final E element) {
-		return set(0, element);
-	}
-	
-	@Override
-	public E poke(final int down, final E element) {
-		return set(down, element);
-	}
-	
-	@Override
-	public void dup() {
-		if (size() == 0) {
-			throw new IllegalStateException("Duplicating the top stack value is not possible when the stack is empty.");
-		}
-		push(peek());
-	}
-	
-	@Override
-	public void swap() {
-		if (size() < 2) {
-			throw new IllegalStateException(
-					"Swapping the two top stack values is not possible when the stack contains lesser than two values.");
-		}
-		Collections.swap(this, 0, 1);
-	}
-	
-	@Override
-	public void takeSnapshot() {
-		snapshots.push(copy());
-	}
-	
-	@Override
-	public void restoreSnapshot() {
-		final Deque<E> snapshot = snapshots.pop();
-		clear();
-		addAll(snapshot);
-	}
-	
-	@Override
-	public void discardSnapshot() {
-		snapshots.pop();
-	}
-	
-	@Override
-	public void removeSnapshot(final boolean restore) {
-		if (restore) {
-			restoreSnapshot();
-		}
-		else {
-			discardSnapshot();
-		}
-	}
-	
-	@Override
-	public void clearAllSnapshots() {
-		snapshots.clear();
-	}
-	
-	@Override
-	public int getSnapshotCount() {
-		return snapshots.size();
-	}
-	
-	@Override
-	public RestorableStack<E> copy() {
-		return new LinkedListRestorableStack<>(this);
-	}
-	
-	private int checkIndex(final int down) {
-		return checkPositionIndex(down, size(), "A 'down' index must be in range.");
-	}
-	
+class LinkedListRestorableStack<E> : LinkedList<E>, RestorableStack<E> {
+
+  companion object {
+    private const val serialVersionUID = 3875323652049358971L
+  }
+
+  private val snapshots: Deque<Deque<E>> = ArrayDeque()
+
+  /**
+   * Default c'tor.
+   */
+  constructor() : super()
+
+  /**
+   * C'tor.
+   *
+   * @param c the initial elements of the stack.
+   */
+  constructor(c: Collection<E>) : super(c)
+
+  override fun push(down: Int, element: E) = add(checkIndex(down), element)
+
+  override fun pop(down: Int): E = removeAt(checkIndex(down))
+
+  override fun <T : E> popAs(type: Class<T>): T = type.cast(pop())
+
+  override fun <T : E> popAs(down: Int, type: Class<T>): T = type.cast(pop(checkIndex(down)))
+
+  override fun peek(down: Int): E = get(checkIndex(down))
+
+  override fun <T : E> peekAs(type: Class<T>): T = type.cast(get(0))
+
+  override fun <T : E> peekAs(down: Int, type: Class<T>): T = type.cast(get(checkIndex(down)))
+
+  override fun poke(element: E): E = set(0, element)
+
+  override fun poke(down: Int, element: E): E = set(down, element)
+
+  override fun dup() {
+    check(size != 0) { "Duplicating the top stack value is not possible when the stack is empty." }
+    push(peek())
+  }
+
+  override fun swap() {
+    check(size >= 2) { "Swapping the two top stack values is not possible when the stack contains lesser than two values." }
+    Collections.swap(this, 0, 1)
+  }
+
+  override fun takeSnapshot() = snapshots.push(copy())
+
+  override fun restoreSnapshot() {
+    val snapshot = snapshots.pop()
+    clear()
+    addAll(snapshot)
+  }
+
+  override fun discardSnapshot() {
+    snapshots.pop()
+  }
+
+  override fun removeSnapshot(restore: Boolean) {
+    if (restore) {
+      restoreSnapshot()
+    } else {
+      discardSnapshot()
+    }
+  }
+
+  override fun clearAllSnapshots() = snapshots.clear()
+
+  override val snapshotCount: Int
+    get() = snapshots.size
+
+  override fun copy(): RestorableStack<E> = LinkedListRestorableStack(this)
+
+  private fun checkIndex(down: Int) = Preconditions.checkPositionIndex(down, size, "A 'down' index must be in range.")
+
 }
