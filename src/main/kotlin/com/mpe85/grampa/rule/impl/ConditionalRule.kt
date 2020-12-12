96f8c5f197a2697415149a7420573f6b560fb4f1
Marco Perazzo <marco.perazzo85@gmail.com>
2020-12-13 00:58:38 +0100

    Converted rules to kotlin

--- src/main/kotlin/com/mpe85/grampa/rule/impl/ConditionalRule.kt
+++ src/main/kotlin/com/mpe85/grampa/rule/impl/ConditionalRule.kt
@@ -1,97 +1,45 @@
-package com.mpe85.grampa.rule.impl;
+package com.mpe85.grampa.rule.impl
 
-import static com.google.common.base.Preconditions.checkNotNull;
-
-import java.util.Objects;
-import java.util.Optional;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import com.google.common.base.MoreObjects.ToStringHelper;
-import com.mpe85.grampa.rule.ActionContext;
-import com.mpe85.grampa.rule.Rule;
-import com.mpe85.grampa.rule.RuleContext;
+import com.google.common.base.MoreObjects.ToStringHelper
+import com.mpe85.grampa.rule.ActionContext
+import com.mpe85.grampa.rule.Rule
+import com.mpe85.grampa.rule.RuleContext
+import java.util.Objects.hash
+import java.util.function.Predicate
 
 /**
  * A rule implementation that runs one of two sub rules, depending on a condition.
- * 
- * @author mpe85
  *
- * @param <T>
- *            the type of the stack elements
+ * @author mpe85
+ * @param T the type of the stack elements
+ * @param condition a condition that is evaluated when the rule is run
+ * @param thenRule a rule to run if the condition evaluates to true
+ * @param elseRule an optional rule to run if the condition evaluates to false
  */
-public class ConditionalRule<T> extends AbstractRule<T> {
-	
-	private final Predicate<ActionContext<T>> condition;
-	private final Rule<T> thenRule;
-	private final Rule<T> elseRule;
-	
-	/**
-	 * C'tor.
-	 * 
-	 * @param condition
-	 *            a condition that is evaluated when the rule is run
-	 * @param thenRule
-	 *            a rule to run if the condition evaluates to true
-	 */
-	public ConditionalRule(
-			final Predicate<ActionContext<T>> condition,
-			final Rule<T> thenRule) {
-		this(condition, thenRule, null);
-	}
-	
-	/**
-	 * C'tor.
-	 * 
-	 * @param condition
-	 *            a condition that is evaluated when the rule is run
-	 * @param thenRule
-	 *            a rule to run if the condition evaluates to true
-	 * @param elseRule
-	 *            an optional rule to run if the condition evaluates to false
-	 */
-	public ConditionalRule(
-			final Predicate<ActionContext<T>> condition,
-			final Rule<T> thenRule,
-			final Rule<T> elseRule) {
-		super(Stream.of(thenRule, elseRule)
-				.filter(Objects::nonNull)
-				.collect(Collectors.toList()));
-		this.condition = checkNotNull(condition, "A 'condition' must not be null.");
-		this.thenRule = checkNotNull(thenRule, "A 'thenRule' must not be null.");
-		this.elseRule = elseRule;
-	}
-	
-	@Override
-	public boolean match(final RuleContext<T> context) {
-		return condition.test(context)
-				? thenRule.match(context)
-				: Optional.ofNullable(elseRule).map(r -> r.match(context)).orElse(true);
-	}
-	
-	@Override
-	public int hashCode() {
-		return Objects.hash(super.hashCode(), condition, thenRule, elseRule);
-	}
-	
-	@Override
-	public boolean equals(final Object obj) {
-		if (obj != null && getClass() == obj.getClass()) {
-			final ConditionalRule<?> other = (ConditionalRule<?>) obj;
-			return super.equals(other)
-					&& Objects.equals(condition, other.condition)
-					&& Objects.equals(thenRule, other.thenRule)
-					&& Objects.equals(elseRule, other.elseRule);
-		}
-		return false;
-	}
-	
-	@Override
-	protected ToStringHelper toStringHelper() {
-		return super.toStringHelper()
-				.add("thenRule", thenRule)
-				.add("elseRule", elseRule);
-	}
-	
+class ConditionalRule<T> @JvmOverloads constructor(
+  private val condition: Predicate<ActionContext<T>>,
+  private val thenRule: Rule<T>,
+  private val elseRule: Rule<T>? = null
+) : AbstractRule<T>(sequenceOf(thenRule, elseRule).filterNotNull().toList()) {
+
+  override fun match(context: RuleContext<T>) =
+    if (condition.test(context)) thenRule.match(context) else elseRule?.match(context) ?: true
+
+  override fun hashCode() = hash(super.hashCode(), condition, thenRule, elseRule)
+
+  override fun equals(obj: Any?): Boolean {
+    if (obj != null && javaClass == obj.javaClass) {
+      val other = obj as ConditionalRule<*>
+      return (super.equals(other)
+          && condition == other.condition
+          && thenRule == other.thenRule
+          && elseRule == other.elseRule)
+    }
+    return false
+  }
+
+  override fun toStringHelper(): ToStringHelper = super.toStringHelper()
+    .add("thenRule", thenRule)
+    .add("elseRule", elseRule)
+
 }
