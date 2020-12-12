758d33e42312c8397ce91d29c8d349827bf6f417
Marco Perazzo <marco.perazzo85@gmail.com>
2020-12-13 00:42:02 +0100

    Converted rules to kotlin

--- src/main/kotlin/com/mpe85/grampa/rule/impl/RegexRule.kt
+++ src/main/kotlin/com/mpe85/grampa/rule/impl/RegexRule.kt
@@ -1,69 +1,43 @@
-package com.mpe85.grampa.rule.impl;
+package com.mpe85.grampa.rule.impl
 
-import java.util.Objects;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import com.google.common.base.MoreObjects.ToStringHelper;
-import com.mpe85.grampa.rule.RuleContext;
+import com.google.common.base.MoreObjects.ToStringHelper
+import com.mpe85.grampa.rule.RuleContext
+import java.util.Objects.hash
+import java.util.regex.Pattern
+import java.util.regex.Pattern.compile
 
 /**
  * A regular expression rule implementation.
- * 
- * @author mpe85
  *
- * @param <T>
- *            the type of the stack elements
+ * @author mpe85
+ * @param T the type of the stack elements
+ * @param pattern a compiled regular expression
  */
-public class RegexRule<T> extends AbstractRule<T> {
-	
-	private final Pattern pattern;
-	
-	/**
-	 * C'tor. Constructs a regex rule using an already compiled {@link Pattern}.
-	 * 
-	 * @param pattern
-	 *            a compiled regular expression
-	 */
-	public RegexRule(final Pattern pattern) {
-		this.pattern = pattern;
-	}
-	
-	/**
-	 * C'tor. Constructs a regex rule using a regex string.
-	 * 
-	 * @param regex
-	 *            a string containing a regular expression
-	 */
-	public RegexRule(final String regex) {
-		pattern = Pattern.compile(regex);
-	}
-	
-	@Override
-	public boolean match(final RuleContext<T> context) {
-		final Matcher regexMatcher = pattern.matcher(context.getRestOfInput());
-		return regexMatcher.lookingAt() && context.advanceIndex(regexMatcher.end());
-	}
-	
-	@Override
-	public int hashCode() {
-		return Objects.hash(super.hashCode(), pattern);
-	}
-	
-	@Override
-	public boolean equals(final Object obj) {
-		if (obj != null && getClass() == obj.getClass()) {
-			final RegexRule<?> other = (RegexRule<?>) obj;
-			return super.equals(other)
-					&& Objects.equals(pattern, other.pattern);
-		}
-		return false;
-	}
-	
-	@Override
-	protected ToStringHelper toStringHelper() {
-		return super.toStringHelper()
-				.add("pattern", pattern);
-	}
-	
+class RegexRule<T>(private val pattern: Pattern) : AbstractRule<T>() {
+
+  /**
+   * C'tor. Constructs a regex rule using a regex string.
+   *
+   * @param regex a string containing a regular expression
+   */
+  constructor(regex: String) : this(compile(regex))
+
+  override fun match(context: RuleContext<T>) = pattern.matcher(context.restOfInput).let { matcher ->
+    matcher.lookingAt() && context.advanceIndex(matcher.end())
+  }
+
+  override fun hashCode() = hash(super.hashCode(), pattern)
+
+  override fun equals(obj: Any?): Boolean {
+    if (obj != null && javaClass == obj.javaClass) {
+      val other = obj as RegexRule<*>
+      return super.equals(other)
+          && pattern == other.pattern
+    }
+    return false
+  }
+
+  override fun toStringHelper(): ToStringHelper = super.toStringHelper()
+    .add("pattern", pattern)
+
 }
--- src/main/kotlin/com/mpe85/grampa/rule/impl/TrieRule.kt
+++ src/main/kotlin/com/mpe85/grampa/rule/impl/TrieRule.kt
@@ -1,135 +1,98 @@
-package com.mpe85.grampa.rule.impl;
+package com.mpe85.grampa.rule.impl
 
-import static com.google.common.base.Preconditions.checkNotNull;
-
-import java.util.Objects;
-import java.util.Set;
-import java.util.stream.Collectors;
-
-import com.google.common.base.MoreObjects.ToStringHelper;
-import com.google.common.collect.Sets;
-import com.google.common.collect.Streams;
-import com.ibm.icu.lang.UCharacter;
-import com.ibm.icu.util.BytesTrie.Result;
-import com.ibm.icu.util.CharsTrie;
-import com.ibm.icu.util.CharsTrieBuilder;
-import com.ibm.icu.util.StringTrieBuilder.Option;
-import com.mpe85.grampa.rule.RuleContext;
+import com.google.common.base.MoreObjects.ToStringHelper
+import com.ibm.icu.lang.UCharacter.toLowerCase
+import com.ibm.icu.util.BytesTrie.Result.FINAL_VALUE
+import com.ibm.icu.util.BytesTrie.Result.INTERMEDIATE_VALUE
+import com.ibm.icu.util.BytesTrie.Result.NO_MATCH
+import com.ibm.icu.util.CharsTrie
+import com.ibm.icu.util.CharsTrieBuilder
+import com.ibm.icu.util.StringTrieBuilder.Option.FAST
+import com.mpe85.grampa.rule.RuleContext
+import java.util.Objects.hash
 
 /**
  * A trie (prefix tree) rule implementation that matches the input against a dictionary.
- * 
- * @author mpe85
  *
- * @param <T>
- *            the type of the stack elements
+ * @author mpe85
+ * @param T the type of the stack elements
+ * @param strings a set of strings
+ * @param ignoreCase if true, the case of the strings is ignored
  */
-public class TrieRule<T> extends AbstractRule<T> {
-	
-	private final CharsTrie trie;
-	private final boolean ignoreCase;
-	
-	/**
-	 * C'tor. Constructs a case-sensitive trie.
-	 * 
-	 * @param strings
-	 *            a variable number of strings
-	 */
-	public TrieRule(final String... strings) {
-		this(false, strings);
-	}
-	
-	/**
-	 * C'tor. Constructs a case-sensitive trie (case-sensitive or case-insensitive).
-	 * 
-	 * @param ignoreCase
-	 *            if true, the case of the strings is ignored
-	 * @param strings
-	 *            a variable number of strings
-	 */
-	public TrieRule(final boolean ignoreCase, final String... strings) {
-		this(Sets.newHashSet(strings), ignoreCase);
-	}
-	
-	/**
-	 * C'tor. Constructs a case-sensitive trie.
-	 * 
-	 * @param strings
-	 *            a set of strings
-	 */
-	public TrieRule(final Set<String> strings) {
-		this(strings, false);
-	}
-	
-	/**
-	 * C'tor. Constructs a case-sensitive trie (case-sensitive or case-insensitive).
-	 * 
-	 * @param ignoreCase
-	 *            if true, the case of the strings is ignored
-	 * @param strings
-	 *            a set of strings
-	 */
-	public TrieRule(
-			final Set<String> strings,
-			final boolean ignoreCase) {
-		final CharsTrieBuilder builder = new CharsTrieBuilder();
-		strings.forEach(s -> {
-			checkNotNull(s, "A 'string' must not be null.");
-			builder.add(ignoreCase ? UCharacter.toLowerCase(s) : s, 0);
-		});
-		trie = builder.build(Option.FAST);
-		this.ignoreCase = ignoreCase;
-	}
-	
-	/**
-	 * Returns all strings inside the trie.
-	 * 
-	 * @return a set of strings
-	 */
-	public Set<String> getStrings() {
-		return Streams.stream(trie.iterator())
-				.map(e -> e.chars.toString())
-				.collect(Collectors.toSet());
-	}
-	
-	@Override
-	public boolean match(final RuleContext<T> context) {
-		int longestMatch = 0;
-		final int[] codePoints = context.getRestOfInput().codePoints().toArray();
-		for (int i = 0; i < codePoints.length; i++) {
-			final Result result = trie.next(ignoreCase ? UCharacter.toLowerCase(codePoints[i]) : codePoints[i]);
-			if (result == Result.FINAL_VALUE || result == Result.INTERMEDIATE_VALUE) {
-				longestMatch = i + 1;
-			}
-			if (result == Result.FINAL_VALUE || result == Result.NO_MATCH) {
-				break;
-			}
-		}
-		trie.reset();
-		return longestMatch > 0 && context.advanceIndex(longestMatch);
-	}
-	
-	@Override
-	public int hashCode() {
-		return Objects.hash(super.hashCode(), getStrings(), ignoreCase);
-	}
-	
-	@Override
-	public boolean equals(final Object obj) {
-		if (obj != null && getClass() == obj.getClass()) {
-			final TrieRule<?> other = (TrieRule<?>) obj;
-			return super.equals(other)
-					&& Objects.equals(getStrings(), other.getStrings())
-					&& Objects.equals(ignoreCase, other.ignoreCase);
-		}
-		return false;
-	}
-	
-	@Override
-	protected ToStringHelper toStringHelper() {
-		return super.toStringHelper()
-				.add("strings", getStrings())
-				.add("ignoreCase", ignoreCase);
-	}
-	
+class TrieRule<T> @JvmOverloads constructor(strings: Set<String>, private val ignoreCase: Boolean = false) :
+  AbstractRule<T>() {
+
+  private val trie: CharsTrie = CharsTrieBuilder().apply {
+    strings.forEach { s ->
+      add(if (ignoreCase) toLowerCase(s) else s, 0)
+    }
+  }.build(FAST)
+
+  /*
+  init {
+    val builder = CharsTrieBuilder()
+    strings.forEach(Consumer { s: String? ->
+      Preconditions.checkNotNull(s, "A 'string' must not be null.")
+      builder.add(if (ignoreCase) UCharacter.toLowerCase(s) else s, 0)
+    })
+    trie = builder.build(StringTrieBuilder.Option.FAST)
+  }*/
+
+  /**
+   * C'tor. Constructs a case-sensitive trie.
+   *
+   * @param strings a variable number of strings
+   */
+  constructor(vararg strings: String) : this(false, *strings)
+
+  /**
+   * C'tor. Constructs a case-sensitive trie (case-sensitive or case-insensitive).
+   *
+   * @param ignoreCase if true, the case of the strings is ignored
+   * @param strings a variable number of strings
+   */
+  constructor(ignoreCase: Boolean, vararg strings: String) : this(strings.toSet(), ignoreCase)
+
+  /**
+   * Returns all strings inside the trie.
+   *
+   * @return a set of strings
+   */
+  private val strings: Set<String>
+    get() = trie.iterator().asSequence()
+      .map { entry -> entry.chars.toString() }
+      .toSet()
+
+  override fun match(context: RuleContext<T>): Boolean {
+    var longestMatch = 0
+    val codePoints = context.restOfInput.codePoints().toArray()
+    for (i in codePoints.indices) {
+      val result = trie.next(if (ignoreCase) toLowerCase(codePoints[i]) else codePoints[i])
+      if (result in setOf(FINAL_VALUE, INTERMEDIATE_VALUE)) {
+        longestMatch = i + 1
+      }
+      if (result in setOf(FINAL_VALUE, NO_MATCH)) {
+        break
+      }
+    }
+    trie.reset()
+    return longestMatch > 0 && context.advanceIndex(longestMatch)
+  }
+
+  override fun hashCode() = hash(super.hashCode(), strings, ignoreCase)
+
+  override fun equals(obj: Any?): Boolean {
+    if (obj != null && javaClass == obj.javaClass) {
+      val other = obj as TrieRule<*>
+      return super.equals(other)
+          && strings == other.strings
+          && ignoreCase == other.ignoreCase
+    }
+    return false
+  }
+
+  override fun toStringHelper(): ToStringHelper = super.toStringHelper()
+    .add("strings", strings)
+    .add("ignoreCase", ignoreCase)
+
 }
