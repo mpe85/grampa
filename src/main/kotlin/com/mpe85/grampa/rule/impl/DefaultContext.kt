206f8025bd326834cae0eaeea1af0c9bdaa0d82a
Marco Perazzo <marco.perazzo85@gmail.com>
2020-12-14 23:33:11 +0100

    Converted default context to kotlin

--- src/main/kotlin/com/mpe85/grampa/rule/impl/DefaultContext.kt
+++ src/main/kotlin/com/mpe85/grampa/rule/impl/DefaultContext.kt
@@ -1,205 +1,116 @@
-package com.mpe85.grampa.rule.impl;
+package com.mpe85.grampa.rule.impl
 
-import static com.google.common.base.Preconditions.checkArgument;
-import static com.google.common.base.Preconditions.checkNotNull;
-import static com.google.common.base.Preconditions.checkPositionIndex;
+import com.google.common.base.Preconditions
+import com.google.common.eventbus.EventBus
+import com.mpe85.grampa.event.MatchFailureEvent
+import com.mpe85.grampa.event.MatchSuccessEvent
+import com.mpe85.grampa.event.PreMatchEvent
+import com.mpe85.grampa.input.InputBuffer
+import com.mpe85.grampa.rule.ActionContext
+import com.mpe85.grampa.rule.Rule
+import com.mpe85.grampa.rule.RuleContext
+import com.mpe85.grampa.util.stack.RestorableStack
 
-import java.util.Optional;
+class DefaultContext<T> @JvmOverloads constructor(
+  private val inputBuffer: InputBuffer,
+  private val level: Int,
+  private val rule: Rule<T>,
+  private val startIndex: Int,
+  private val stack: RestorableStack<T>,
+  private val bus: EventBus,
+  private val parentContext: RuleContext<T>? = null
+) : RuleContext<T>, ActionContext<T> {
 
-import com.google.common.eventbus.EventBus;
-import com.mpe85.grampa.event.MatchFailureEvent;
-import com.mpe85.grampa.event.MatchSuccessEvent;
-import com.mpe85.grampa.event.PreMatchEvent;
-import com.mpe85.grampa.input.InputBuffer;
-import com.mpe85.grampa.input.InputPosition;
-import com.mpe85.grampa.rule.ActionContext;
-import com.mpe85.grampa.rule.Rule;
-import com.mpe85.grampa.rule.RuleContext;
-import com.mpe85.grampa.util.stack.RestorableStack;
+  private var currentIndex = startIndex
+  private var cachedCurrentChar: Char? = null
+  private var cachedCurrentCodePoint: Int? = null
+  private var previousMatch = parentContext?.previousMatch
 
-public class DefaultContext<T> implements RuleContext<T>, ActionContext<T> {
-	
-	private final InputBuffer inputBuffer;
-	private final int level;
-	private final Rule<T> rule;
-	private final int startIndex;
-	private final RestorableStack<T> stack;
-	private final EventBus bus;
-	
-	private int currentIndex;
-	private Character cachedCurrentChar;
-	private Integer cachedCurrentCodePoint;
-	private CharSequence previousMatch;
-	private RuleContext<T> parentContext;
-	
-	public DefaultContext(
-			final InputBuffer inputBuffer,
-			final int level,
-			final Rule<T> rule,
-			final int startIndex,
-			final RestorableStack<T> stack,
-			final EventBus bus) {
-		this(inputBuffer, level, rule, startIndex, stack, bus, null);
-	}
-	
-	public DefaultContext(
-			final InputBuffer inputBuffer,
-			final int level,
-			final Rule<T> rule,
-			final int startIndex,
-			final RestorableStack<T> stack,
-			final EventBus bus,
-			final RuleContext<T> parentContext) {
-		this.inputBuffer = checkNotNull(inputBuffer, "An 'inputBuffer' must not be null.");
-		this.level = level;
-		this.rule = checkNotNull(rule, "A 'rule' must not be null.");
-		this.startIndex = startIndex;
-		this.currentIndex = startIndex;
-		this.stack = checkNotNull(stack, "A 'stack' must not be null.");
-		this.bus = checkNotNull(bus, "A 'bus' must not be null.");
-		this.parentContext = parentContext;
-		this.previousMatch = Optional.ofNullable(parentContext).map(RuleContext::getPreviousMatch).orElse(null);
-	}
-	
-	@Override
-	public int getLevel() {
-		return level;
-	}
-	
-	@Override
-	public int getStartIndex() {
-		return startIndex;
-	}
-	
-	@Override
-	public int getCurrentIndex() {
-		return currentIndex;
-	}
-	
-	@Override
-	public boolean isAtEndOfInput() {
-		return currentIndex == inputBuffer.getLength();
-	}
-	
-	@Override
-	public char getCurrentChar() {
-		if (cachedCurrentChar == null) {
-			cachedCurrentChar = inputBuffer.getChar(currentIndex);
-		}
-		return cachedCurrentChar;
-	}
-	
-	@Override
-	public int getCurrentCodePoint() {
-		if (cachedCurrentCodePoint == null) {
-			cachedCurrentCodePoint = inputBuffer.getCodePoint(currentIndex);
-		}
-		return cachedCurrentCodePoint;
-	}
-	
-	@Override
-	public int getNumberOfCharsLeft() {
-		return inputBuffer.getLength() - currentIndex;
-	}
-	
-	@Override
-	public CharSequence getInput() {
-		return inputBuffer.subSequence(0, inputBuffer.getLength());
-	}
-	
-	@Override
-	public CharSequence getMatchedInput() {
-		return inputBuffer.subSequence(0, currentIndex);
-	}
-	
-	@Override
-	public CharSequence getRestOfInput() {
-		return inputBuffer.subSequence(currentIndex, inputBuffer.getLength());
-	}
-	
-	@Override
-	public CharSequence getPreviousMatch() {
-		return previousMatch;
-	}
-	
-	@Override
-	public InputPosition getPosition() {
-		return inputBuffer.getPosition(currentIndex);
-	}
-	
-	@Override
-	public boolean inPredicate() {
-		return rule.isPredicate()
-				|| Optional.ofNullable(parentContext).map(RuleContext::inPredicate).orElse(false);
-	}
-	
-	@Override
-	public RestorableStack<T> getStack() {
-		return stack;
-	}
-	
-	@Override
-	public RuleContext<T> getParent() {
-		return parentContext;
-	}
-	
-	@Override
-	public void post(final Object event) {
-		bus.post(checkNotNull(event, "An 'event' must not be null."));
-	}
-	
-	@Override
-	public InputBuffer getInputBuffer() {
-		return inputBuffer;
-	}
-	
-	@Override
-	public void setCurrentIndex(final int currentIndex) {
-		checkPositionIndex(currentIndex, inputBuffer.getLength(), "A 'currentIndex' must be in range.");
-		if (currentIndex > this.currentIndex) {
-			previousMatch = inputBuffer.subSequence(this.currentIndex, currentIndex);
-		}
-		invalidateCache();
-		this.currentIndex = currentIndex;
-	}
-	
-	@Override
-	public boolean advanceIndex(final int delta) {
-		checkArgument(delta >= 0, "A 'delta' must be greater or equal 0.");
-		if (currentIndex + delta <= inputBuffer.getLength()) {
-			currentIndex += delta;
-			invalidateCache();
-			return true;
-		}
-		return false;
-	}
-	
-	@Override
-	public boolean run() {
-		stack.takeSnapshot();
-		bus.post(new PreMatchEvent<>(this));
-		final boolean matched = rule.match(this);
-		if (matched && parentContext != null) {
-			parentContext.setCurrentIndex(currentIndex);
-		}
-		if (matched) {
-			bus.post(new MatchSuccessEvent<>(this));
-		}
-		else {
-			bus.post(new MatchFailureEvent<>(this));
-		}
-		stack.removeSnapshot(!matched);
-		return matched;
-	}
-	
-	@Override
-	public RuleContext<T> createChildContext(final Rule<T> rule) {
-		return new DefaultContext<>(inputBuffer, level + 1, rule, currentIndex, stack, bus, this);
-	}
-	
-	private void invalidateCache() {
-		cachedCurrentChar = null;
-		cachedCurrentCodePoint = null;
-	}
-	
+  override fun getLevel() = level
+
+  override fun getStartIndex() = startIndex
+
+  override fun getCurrentIndex() = currentIndex
+
+  override fun isAtEndOfInput() = currentIndex == inputBuffer.length
+
+  override fun getCurrentChar(): Char {
+    if (cachedCurrentChar == null) {
+      cachedCurrentChar = inputBuffer.getChar(currentIndex)
+    }
+    return cachedCurrentChar as Char
+  }
+
+  override fun getCurrentCodePoint(): Int {
+    if (cachedCurrentCodePoint == null) {
+      cachedCurrentCodePoint = inputBuffer.getCodePoint(currentIndex)
+    }
+    return cachedCurrentCodePoint as Int
+  }
+
+  override fun getNumberOfCharsLeft() = inputBuffer.length - currentIndex
+
+  override fun getInput() = inputBuffer.subSequence(0, inputBuffer.length)
+
+  override fun getMatchedInput() = inputBuffer.subSequence(0, currentIndex)
+
+  override fun getRestOfInput() = inputBuffer.subSequence(currentIndex, inputBuffer.length)
+
+  override fun getPreviousMatch() = previousMatch
+
+  override fun getPosition() = inputBuffer.getPosition(currentIndex)
+
+  override fun inPredicate() = rule.isPredicate || parentContext?.inPredicate() ?: false
+
+  override fun getStack() = stack
+
+  override fun getParent() = parentContext
+
+  override fun post(event: Any) = bus.post(event)
+
+  override fun getInputBuffer() = inputBuffer
+
+  override fun setCurrentIndex(currentIndex: Int) {
+    Preconditions.checkPositionIndex(currentIndex, inputBuffer.length, "A 'currentIndex' must be in range.")
+    if (currentIndex > this.currentIndex) {
+      previousMatch = inputBuffer.subSequence(this.currentIndex, currentIndex)
+    }
+    invalidateCache()
+    this.currentIndex = currentIndex
+  }
+
+  override fun advanceIndex(delta: Int): Boolean {
+    Preconditions.checkArgument(delta >= 0, "A 'delta' must be greater or equal 0.")
+    if (currentIndex + delta <= inputBuffer.length) {
+      currentIndex += delta
+      invalidateCache()
+      return true
+    }
+    return false
+  }
+
+  override fun run(): Boolean {
+    stack.takeSnapshot()
+    bus.post(PreMatchEvent(this))
+    val matched = rule.match(this)
+    if (matched && parentContext != null) {
+      parentContext.currentIndex = currentIndex
+    }
+    if (matched) {
+      bus.post(MatchSuccessEvent(this))
+    } else {
+      bus.post(MatchFailureEvent(this))
+    }
+    stack.removeSnapshot(!matched)
+    return matched
+  }
+
+  override fun createChildContext(rule: Rule<T>) =
+    DefaultContext(inputBuffer, level + 1, rule, currentIndex, stack, bus, this)
+
+  private fun invalidateCache() {
+    cachedCurrentChar = null
+    cachedCurrentCodePoint = null
+  }
+
 }
--- src/main/kotlin/com/mpe85/grampa/runner/DefaultParseRunner.kt
+++ src/main/kotlin/com/mpe85/grampa/runner/DefaultParseRunner.kt
@@ -8,6 +8,7 @@ import com.mpe85.grampa.event.PreParseEvent
 import com.mpe85.grampa.input.InputBuffer
 import com.mpe85.grampa.input.impl.CharSequenceInputBuffer
 import com.mpe85.grampa.parser.Parser
+import com.mpe85.grampa.rule.Rule
 import com.mpe85.grampa.rule.impl.DefaultContext
 import com.mpe85.grampa.util.stack.RestorableStack
 import com.mpe85.grampa.util.stack.impl.LinkedListRestorableStack
@@ -16,7 +17,6 @@ import com.mpe85.grampa.util.stack.impl.LinkedListRestorableStack
  * The default parse runner. May be overridden by a custom implementation.
  *
  * @author mpe85
- *
  * @param T the type of the stack elements
  * @param parser a parser instance
  * @param handler a handler for parser events
@@ -25,31 +25,32 @@ open class DefaultParseRunner<T> @JvmOverloads constructor(
   parser: Parser<T>,
   handler: SubscriberExceptionHandler? = null
 ) {
+
   /**
-   * Gets the root rule of the parser.
+   * Get the root rule of the parser.
    *
    * @return the root rule
    */
-  val rootRule = parser.root()
+  val rootRule: Rule<T> = parser.root()
   private val bus = handler?.let { EventBus(it) } ?: EventBus()
   private var valueStack: RestorableStack<T>? = null
 
   /**
-   * Registers a listener to the parser event bus.
+   * Register a listener to the parser event bus.
    *
    * @param listener a parse event listener
    */
   fun registerListener(listener: ParseEventListener<T>) = bus.register(listener)
 
   /**
-   * Unregisters a listener to the parser event bus.
+   * Unregister a listener to the parser event bus.
    *
    * @param listener a parse event listener
    */
   fun unregisterListener(listener: ParseEventListener<T>) = bus.unregister(listener)
 
   /**
-   * Runs the parser against a character sequence.
+   * Run the parser against a character sequence.
    *
    * @param charSequence a character sequence
    * @return the parse result
@@ -57,7 +58,7 @@ open class DefaultParseRunner<T> @JvmOverloads constructor(
   fun run(charSequence: CharSequence?) = run(CharSequenceInputBuffer(charSequence!!))
 
   /**
-   * Runs the parser against an input buffer.
+   * Run the parser against an input buffer.
    *
    * @param inputBuffer an input buffer
    * @return the parse result
@@ -73,16 +74,16 @@ open class DefaultParseRunner<T> @JvmOverloads constructor(
   }
 
   /**
-   * Creates the initial root context for the parser's root rule.
+   * Create the initial root context for the parser's root rule.
    *
    * @param inputBuffer an input buffer
    * @return a rule context
    */
-  protected fun createRootContext(inputBuffer: InputBuffer?) =
-    DefaultContext(inputBuffer, 0, rootRule, 0, valueStack, bus)
+  protected fun createRootContext(inputBuffer: InputBuffer) =
+    DefaultContext(inputBuffer, 0, rootRule, 0, valueStack!!, bus)
 
   /**
-   * Resets (clears) the stack.
+   * Reset (clear) the stack.
    */
   private fun resetStack() {
     valueStack = LinkedListRestorableStack()
