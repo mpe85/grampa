4f9c0afcc0207b22b62ccb70e05dbcdf1aec303f
Marco Perazzo <marco.perazzo85@gmail.com>
2020-12-19 00:19:45 +0100

    Converted test to kotlin

--- src/test/kotlin/com/mpe85/grampa/GrampaTest.kt
+++ src/test/kotlin/com/mpe85/grampa/GrampaTest.kt
@@ -1,116 +1,125 @@
-package com.mpe85.grampa;
+package com.mpe85.grampa
 
-import com.mpe85.grampa.exception.ParserCreateException;
-import com.mpe85.grampa.input.impl.FinalRuleMethodTestParser;
-import com.mpe85.grampa.rule.Rule;
-import com.mpe85.grampa.rule.impl.CharPredicateRule;
-import com.mpe85.grampa.rule.impl.EmptyRule;
-import com.mpe85.grampa.rule.impl.FirstOfRule;
-import com.mpe85.grampa.rule.impl.SequenceRule;
-import org.junit.jupiter.api.Test;
+import com.mpe85.grampa.Grampa.createParser
+import com.mpe85.grampa.exception.ParserCreateException
+import com.mpe85.grampa.input.impl.FinalRuleMethodTestParser
+import com.mpe85.grampa.rule.Rule
+import com.mpe85.grampa.rule.impl.CharPredicateRule
+import com.mpe85.grampa.rule.impl.EmptyRule
+import com.mpe85.grampa.rule.impl.FirstOfRule
+import com.mpe85.grampa.rule.impl.SequenceRule
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
 
-import static org.junit.jupiter.api.Assertions.*;
+class GrampaTest {
+  @Test
+  fun createParser_valid_noArgs() {
+    val p = createParser(TestParser::class.java)
+    Assertions.assertNull(p.dummy)
+    val root = p.root()
+    verifyTestParserRules(root)
+  }
 
-public class GrampaTest {
+  @Test
+  fun createParser_invalid_privateRuleMethod() {
+    Assertions.assertThrows(
+      ParserCreateException::class.java
+    ) { createParser(InvalidParser::class.java) }
+  }
 
-    @Test
-    public void createParser_valid_noArgs() {
-        final TestParser p = Grampa.createParser(TestParser.class);
-        assertNull(p.getDummy());
+  @Test
+  fun createParser_valid_withArgs() {
+    val p = createParser(TestParser::class.java, String::class.java).withArgs("foo")
+    Assertions.assertEquals("foo", p.dummy)
+    val root = p.root()
+    verifyTestParserRules(root)
+  }
 
-        final Rule<String> root = p.root();
-        verifyTestParserRules(root);
+  @Test
+  fun createParser_invalid_ctor() {
+    Assertions.assertThrows(
+      ParserCreateException::class.java
+    ) { createParser(TestParser::class.java, Int::class.java).withArgs(4711) }
+  }
+
+  @Test
+  fun createParser_invalid_args() {
+    Assertions.assertThrows(
+      ParserCreateException::class.java
+    ) { createParser(TestParser::class.java, String::class.java).withArgs(4711) }
+  }
+
+  @Test
+  fun createParser_invalid_noDefaultCtor() {
+    Assertions.assertThrows(
+      ParserCreateException::class.java
+    ) {
+      createParser(
+        NoDefaultCtorTestParser::class.java
+      )
     }
+  }
 
-    @Test
-    public void createParser_invalid_privateRuleMethod() {
-        assertThrows(
-                ParserCreateException.class,
-                () -> Grampa.createParser(InvalidParser.class));
+  @Test
+  fun createParser_invalid_finalRuleMethod() {
+    Assertions.assertThrows(
+      ParserCreateException::class.java
+    ) {
+      createParser(
+        FinalRuleMethodTestParser::class.java
+      )
     }
+  }
 
-    @Test
-    public void createParser_valid_withArgs() {
-        final TestParser p = Grampa.createParser(TestParser.class, String.class).withArgs("foo");
-        assertEquals("foo", p.getDummy());
-
-        final Rule<String> root = p.root();
-        verifyTestParserRules(root);
+  @Test
+  fun createParser_invalid_staticRuleMethod() {
+    Assertions.assertThrows(
+      ParserCreateException::class.java
+    ) {
+      createParser(
+        StaticRuleMethodTestParser::class.java
+      )
     }
+  }
 
-    @Test
-    public void createParser_invalid_ctor() {
-        assertThrows(
-                ParserCreateException.class,
-                () -> Grampa.createParser(TestParser.class, Integer.class).withArgs(4711));
+  @Test
+  fun createParser_valid_inheritance() {
+    val superParser = createParser(SuperParser::class.java)
+    verifySuperParserRules(superParser.root())
+    val subParser = createParser(SubParser::class.java)
+    verifySuperParserRules(subParser.root())
+  }
+
+  private fun verifyTestParserRules(root: Rule<String>) {
+    Assertions.assertTrue(root is FirstOfRule<*>)
+    Assertions.assertEquals(2, root.children.size)
+    Assertions.assertTrue(root.children[0] is CharPredicateRule<*>)
+    Assertions.assertTrue(root.children[1] is SequenceRule<*>)
+    val sequenceRule = root.children[1] as SequenceRule<String>
+    Assertions.assertEquals(6, sequenceRule.children.size)
+    Assertions.assertTrue(sequenceRule.children[0] is EmptyRule<*>)
+    Assertions.assertTrue(sequenceRule.children[1] is EmptyRule<*>)
+    Assertions.assertTrue(sequenceRule.children[2] is EmptyRule<*>)
+    Assertions.assertTrue(sequenceRule.children[3] is CharPredicateRule<*>)
+    Assertions.assertTrue(sequenceRule.children[4] is FirstOfRule<*>)
+    Assertions.assertTrue(sequenceRule.children[5] is CharPredicateRule<*>)
+    Assertions.assertEquals(root, sequenceRule.children[4])
+  }
+
+  @Test
+  fun createParser_invalid_vararg() {
+    Assertions.assertThrows(ParserCreateException::class.java) {
+      createParser(
+        VarArgsTestParser::class.java
+      )
     }
+  }
 
-    @Test
-    public void createParser_invalid_args() {
-        assertThrows(
-                ParserCreateException.class,
-                () -> Grampa.createParser(TestParser.class, String.class).withArgs(4711));
-    }
-
-    @Test
-    public void createParser_invalid_noDefaultCtor() {
-        assertThrows(
-                ParserCreateException.class,
-                () -> Grampa.createParser(NoDefaultCtorTestParser.class));
-    }
-
-    @Test
-    public void createParser_invalid_finalRuleMethod() {
-        assertThrows(
-                ParserCreateException.class,
-                () -> Grampa.createParser(FinalRuleMethodTestParser.class));
-    }
-
-    @Test
-    public void createParser_invalid_staticRuleMethod() {
-        assertThrows(
-                ParserCreateException.class,
-                () -> Grampa.createParser(StaticRuleMethodTestParser.class));
-    }
-
-    @Test
-    public void createParser_valid_inheritance() {
-        final SuperParser superParser = Grampa.createParser(SuperParser.class);
-        verifySuperParserRules(superParser.root());
-
-        final SubParser subParser = Grampa.createParser(SubParser.class);
-        verifySuperParserRules(subParser.root());
-    }
-
-    private void verifyTestParserRules(final Rule<String> root) {
-        assertTrue(root instanceof FirstOfRule);
-        assertEquals(2, root.getChildren().size());
-        assertTrue(root.getChildren().get(0) instanceof CharPredicateRule);
-        assertTrue(root.getChildren().get(1) instanceof SequenceRule);
-
-        final SequenceRule<String> sequenceRule = (SequenceRule<String>) root.getChildren().get(1);
-        assertEquals(6, sequenceRule.getChildren().size());
-        assertTrue(sequenceRule.getChildren().get(0) instanceof EmptyRule);
-        assertTrue(sequenceRule.getChildren().get(1) instanceof EmptyRule);
-        assertTrue(sequenceRule.getChildren().get(2) instanceof EmptyRule);
-        assertTrue(sequenceRule.getChildren().get(3) instanceof CharPredicateRule);
-        assertTrue(sequenceRule.getChildren().get(4) instanceof FirstOfRule);
-        assertTrue(sequenceRule.getChildren().get(5) instanceof CharPredicateRule);
-
-        assertEquals(root, sequenceRule.getChildren().get(4));
-    }
-
-    @Test
-    public void createParser_invalid_vararg() {
-        assertThrows(ParserCreateException.class, () -> Grampa.createParser(VarArgsTestParser.class));
-    }
-
-    private void verifySuperParserRules(final Rule<String> root) {
-        assertTrue(root instanceof SequenceRule);
-        assertEquals(2, root.getChildren().size());
-        assertTrue(root.getChildren().get(0) instanceof CharPredicateRule);
-        assertTrue(root.getChildren().get(1) instanceof SequenceRule);
-
-        assertEquals(root, root.getChildren().get(1));
-    }
+  private fun verifySuperParserRules(root: Rule<String>) {
+    Assertions.assertTrue(root is SequenceRule<*>)
+    Assertions.assertEquals(2, root.children.size)
+    Assertions.assertTrue(root.children[0] is CharPredicateRule<*>)
+    Assertions.assertTrue(root.children[1] is SequenceRule<*>)
+    Assertions.assertEquals(root, root.children[1])
+  }
 }
