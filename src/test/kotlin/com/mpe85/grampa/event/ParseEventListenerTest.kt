5ffd4abc97d70fa5e6ae3ba79abe9cdc5afd3b9f
Marco Perazzo <marco.perazzo85@gmail.com>
2020-12-23 00:23:26 +0100

    Converted test to kotest

--- src/test/kotlin/com/mpe85/grampa/event/ParseEventListenerTest.kt
+++ src/test/kotlin/com/mpe85/grampa/event/ParseEventListenerTest.kt
@@ -1,67 +1,52 @@
 package com.mpe85.grampa.event
 
 import com.mpe85.grampa.rule.RuleContext
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
 import io.mockk.mockk
-import kotlin.test.Test
-import kotlin.test.assertEquals
-import kotlin.test.assertNotNull
-import kotlin.test.assertTrue
 import org.greenrobot.eventbus.EventBus
 import org.greenrobot.eventbus.Subscribe
 import org.greenrobot.eventbus.SubscriberExceptionEvent
-import org.junit.jupiter.api.TestInstance
-import org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS
 
-@SuppressFBWarnings(
-  value = ["SIC_INNER_SHOULD_BE_STATIC_ANON"],
-  justification = "Performance is not of great importance in unit tests."
-)
-@TestInstance(PER_CLASS)
-class ParseEventListenerTest {
-
-  @Test
-  fun postEvent_valid() {
-    class TestListener : ParseEventListener<String>() {
+class ParseEventListenerTests : StringSpec({
+  "Listens to event" {
+    val listener = object : ParseEventListener<String>() {
+      var called = false
       override fun beforeParse(event: PreParseEvent<String>) {
         called = true
       }
-
-      var called = false
     }
 
-    val listener = TestListener()
-    val bus = EventBus.builder().logNoSubscriberMessages(false).build()
-    bus.register(listener)
-    bus.post(PreParseEvent(mockk<RuleContext<String>>(relaxed = true)))
-    assertTrue(listener.called)
-  }
+    EventBus.builder().logNoSubscriberMessages(false).build().apply {
+      register(listener)
+      post(PreParseEvent(mockk<RuleContext<String>>(relaxed = true)))
+    }
 
-  @Test
-  fun postEvent_exception() {
+    listener.called shouldBe true
+  }
+  "Handles subscriber exception" {
     val ex = RuntimeException("failure")
 
-
-    class TestListener : ParseEventListener<String>() {
+    val listener = object : ParseEventListener<String>() {
+      var exEvent: SubscriberExceptionEvent? = null
       override fun beforeParse(event: PreParseEvent<String>) = throw ex
 
       @Subscribe
       fun exHandler(event: SubscriberExceptionEvent) {
         exEvent = event
       }
-
-      var exEvent: SubscriberExceptionEvent? = null
     }
 
-    val listener = TestListener()
-    val bus = EventBus.builder().logNoSubscriberMessages(false).build()
     val event = PreParseEvent(mockk<RuleContext<String>>(relaxed = true))
-    bus.register(listener)
-    bus.post(event)
+    EventBus.builder().logNoSubscriberMessages(false).build().apply {
+      register(listener)
+      post(event)
+    }
 
-    assertNotNull(listener.exEvent)
-    assertEquals(listener, listener.exEvent?.causingSubscriber)
-    assertEquals(event, listener.exEvent?.causingEvent)
-    assertEquals(ex, listener.exEvent?.throwable)
+    listener.exEvent shouldNotBe null
+    listener.exEvent?.causingSubscriber shouldBe listener
+    listener.exEvent?.causingEvent shouldBe event
+    listener.exEvent?.throwable shouldBe ex
   }
-}
+})
