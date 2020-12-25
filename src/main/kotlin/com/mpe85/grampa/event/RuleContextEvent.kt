e3765ff06281914073bf97a11f334daf98c5de29
Marco Perazzo <marco.perazzo85@gmail.com>
2020-12-25 11:48:35 +0100

    Refactored parse events

--- src/main/kotlin/com/mpe85/grampa/event/RuleContextEvent.kt
+++ src/main/kotlin/com/mpe85/grampa/event/RuleContextEvent.kt
@@ -9,4 +9,40 @@ import com.mpe85.grampa.rule.RuleContext
  * @param[T] The type of the stack elements
  * @property[context] The rule context in which the event was posted
  */
-abstract class RuleContextEvent<T>(val context: RuleContext<T>)
+sealed class RuleContextEvent<T>(val context: RuleContext<T>)
+
+/**
+ * Event posted before a parse run.
+ *
+ * @author mpe85
+ * @param[T] The type of the stack elements
+ * @param[context] The rule context in which the event was posted
+ */
+class PreParseEvent<T>(context: RuleContext<T>) : RuleContextEvent<T>(context)
+
+/**
+ * Event posted before a rule match.
+ *
+ * @author mpe85
+ * @param[T] The type of the stack elements
+ * @param[context] The rule context in which the event was posted
+ */
+class PreMatchEvent<T>(context: RuleContext<T>) : RuleContextEvent<T>(context)
+
+/**
+ * Event posted on a match success.
+ *
+ * @author mpe85
+ * @param[T] The type of the stack elements
+ * @param[context] The rule context in which the event was posted
+ */
+class MatchSuccessEvent<T>(context: RuleContext<T>) : RuleContextEvent<T>(context)
+
+/**
+ * Event posted on a match failure.
+ *
+ * @author mpe85
+ * @param[T] The type of the stack elements
+ * @param[context] The rule context in which the event was posted
+ */
+class MatchFailureEvent<T>(context: RuleContext<T>) : RuleContextEvent<T>(context)
