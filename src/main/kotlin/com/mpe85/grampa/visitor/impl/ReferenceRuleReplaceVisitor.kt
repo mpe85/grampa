82515c7f36216bda13319aaae1da3de501132e48
Marco Perazzo <marco.perazzo85@gmail.com>
2020-12-31 23:12:10 +0100

    Refactoring

--- src/main/kotlin/com/mpe85/grampa/visitor/impl/ReferenceRuleReplaceVisitor.kt
+++ src/main/kotlin/com/mpe85/grampa/visitor/impl/ReferenceRuleReplaceVisitor.kt
@@ -1,5 +1,6 @@
 package com.mpe85.grampa.visitor.impl
 
+import com.mpe85.grampa.intercept.RuleMethodInterceptor
 import com.mpe85.grampa.rule.ReferenceRule
 import com.mpe85.grampa.rule.Rule
 import com.mpe85.grampa.rule.impl.AbstractRule
@@ -10,18 +11,15 @@ import com.mpe85.grampa.visitor.RuleVisitor
  *
  * @author mpe85
  * @param[T] The type of the parser stack values
- * @param[replacementRules] A map containing the replacement rules, hashed by the hash code of the rule methods
- *                          they were created by (see [RuleMethodInterceptor]).
+ * @param[replacementRules] The replacement rules, hashed by the hash code of the rule methods they were created by,
+ *   see [RuleMethodInterceptor].
  */
 class ReferenceRuleReplaceVisitor<T>(private val replacementRules: Map<Int, Rule<T>?>) : RuleVisitor<T> {
 
-
-  override fun visit(rule: AbstractRule<T>) {
-    rule.children.forEachIndexed { index, childRule ->
-      (childRule as? ReferenceRule<T>)?.let { refRule ->
-        replacementRules[refRule.referencedRuleHash]?.let { replRule -> rule.replaceReferenceRule(index, replRule) }
-      } ?: childRule.accept(this)
-    }
+  override fun visit(rule: AbstractRule<T>) = rule.children.forEachIndexed { index, childRule ->
+    (childRule as? ReferenceRule<T>)?.let { refRule ->
+      replacementRules[refRule.referencedRuleHash]?.let { rule.replaceReferenceRule(index, it) }
+    } ?: childRule.accept(this)
   }
 
 }
