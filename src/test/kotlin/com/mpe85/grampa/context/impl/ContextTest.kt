ac884f1e4f1ad2b97dde6cb63792f45e43dbeab8
Marco Perazzo <marco.perazzo85@gmail.com>
2021-01-06 00:35:31 +0100

    Converted tests to kotest

--- src/test/kotlin/com/mpe85/grampa/context/impl/ContextTest.kt
+++ src/test/kotlin/com/mpe85/grampa/context/impl/ContextTest.kt
@@ -1,25 +1,14 @@
 package com.mpe85.grampa.context.impl
 
-import com.mpe85.grampa.input.InputBuffer
-import com.mpe85.grampa.rule.Rule
-import com.mpe85.grampa.stack.RestorableStack
-import org.greenrobot.eventbus.EventBus
-import org.junit.jupiter.api.Assertions.assertThrows
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
-import org.mockito.Mock
-import org.mockito.junit.jupiter.MockitoExtension
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.StringSpec
+import io.mockk.mockk
 
-@ExtendWith(MockitoExtension::class)
-class ContextTest {
-  @Test
-  fun advanceIndex_invalid(
-    @Mock ib: InputBuffer,
-    @Mock rule: Rule<String>,
-    @Mock stack: RestorableStack<String>,
-    @Mock bus: EventBus
-  ) {
-    val ctx = Context(ContextState(ib, 0, rule, 0, stack, bus))
-    assertThrows(IllegalArgumentException::class.java) { ctx.advanceIndex(-1) }
+class ContextTests : StringSpec({
+  "Advance index" {
+    Context<String>(ContextState(mockk(), 0, mockk(), 0, mockk(), mockk())).apply {
+      shouldThrow<IllegalArgumentException> { advanceIndex(-1) }
+    }
   }
-}
+})
+
