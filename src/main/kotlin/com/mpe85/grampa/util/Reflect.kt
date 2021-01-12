3081d6ec9eee5aeff69b01f956f0500ca019dad3
Marco Perazzo <marco.perazzo85@gmail.com>
2021-01-13 00:12:39 +0100

    Added docs

--- src/main/kotlin/com/mpe85/grampa/util/Reflect.kt
+++ src/main/kotlin/com/mpe85/grampa/util/Reflect.kt
@@ -1,16 +1,66 @@
 package com.mpe85.grampa.util
 
 import java.lang.reflect.Method
-import java.lang.reflect.Modifier
+import java.lang.reflect.Modifier.isFinal
+import java.lang.reflect.Modifier.isProtected
+import java.lang.reflect.Modifier.isPublic
+import java.lang.reflect.Modifier.isStatic
 import kotlin.reflect.KFunction
-import kotlin.reflect.KVisibility
+import kotlin.reflect.KVisibility.PROTECTED
+import kotlin.reflect.KVisibility.PUBLIC
 
-internal fun Method.isPublic() = Modifier.isPublic(modifiers)
-internal fun Method.isProtected() = Modifier.isProtected(modifiers)
+/**
+ * Check if a [Method] is public.
+ *
+ * @return true if the method is public
+ */
+internal fun Method.isPublic() = isPublic(modifiers)
+
+/**
+ * Check if a [Method] is protected.
+ *
+ * @return true if the method is protected
+ */
+internal fun Method.isProtected() = isProtected(modifiers)
+
+/**
+ * Check if a [Method] is public or protected.
+ *
+ * @return true if the method is either public or protected
+ */
 internal fun Method.isPublicOrProtected() = isPublic() || isProtected()
-internal fun Method.isFinal() = Modifier.isFinal(modifiers)
-internal fun Method.isStatic() = Modifier.isStatic(modifiers)
 
-internal fun KFunction<*>.isPublic() = visibility == KVisibility.PUBLIC
-internal fun KFunction<*>.isProtected() = visibility == KVisibility.PROTECTED
+/**
+ * Check if a [Method] is final.
+ *
+ * @return true if the method is final
+ */
+internal fun Method.isFinal() = isFinal(modifiers)
+
+/**
+ * Check if a [Method] is static.
+ *
+ * @return true if the method is static
+ */
+internal fun Method.isStatic() = isStatic(modifiers)
+
+/**
+ * Check if a [KFunction] is public.
+ *
+ * @return true if the function is public
+ */
+internal fun KFunction<*>.isPublic() = visibility == PUBLIC
+
+/**
+ * Check if a [KFunction] is protected.
+ *
+ * @return true if the function is protected
+ */
+internal fun KFunction<*>.isProtected() = visibility == PROTECTED
+
+/**
+ * Check if a [KFunction] is public or protected.
+ *
+ * @return true if the function is either public or protected
+ */
 internal fun KFunction<*>.isPublicOrProtected() = isPublic() || isProtected()
