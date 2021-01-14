1a234a86fec0f4ab46dbb4f092b6af15805bada7
Marco Perazzo <marco.perazzo85@gmail.com>
2021-01-14 23:42:49 +0100

    Bumped mockk

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -8,7 +8,7 @@ object Versions {
     const val icu4j = "68.2"
     const val kotest = "4.3.2"
     const val kotlin = "1.4.21"
-    const val mockk = "1.10.4"
+    const val mockk = "1.10.5"
     const val spotbugs = "4.2.0"
     const val spotbugsPlugin = "4.6.0"
     const val versions = "0.36.0"
