efa34579750b27f51a8d51aa08103ec8401dc6a7
Marco Perazzo <marco.perazzo85@gmail.com>
2021-02-02 00:30:15 +0100

    CharRangeRuleTests

--- src/test/kotlin/com/mpe85/grampa/grammar/CharRangeRuleTests.kt
+++ src/test/kotlin/com/mpe85/grampa/grammar/CharRangeRuleTests.kt
@@ -3,6 +3,9 @@ package com.mpe85.grampa.grammar
 import com.mpe85.grampa.parser.Parser
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
+import io.kotest.property.Arb
+import io.kotest.property.arbitrary.char
+import io.kotest.property.arbitrary.set
 import io.kotest.property.checkAll
 
 class CharRangeRuleTests : StringSpec({
@@ -19,18 +22,29 @@ class CharRangeRuleTests : StringSpec({
             }
         }
     }
-    "CharRange rule does not match character out of range" {
-        checkAll<Char, Char, Char> { ch1, ch2, ch3 ->
-            val chars = listOf(ch1, ch2, ch3).sorted()
-            if (chars[1] != chars[0] && chars[1] != chars[2]) {
-                Parser(object : AbstractGrammar<Int>() {
-                    override fun root() = charRange(chars[0], chars[1])
-                }).run("${chars[2]}").apply {
-                    matched shouldBe false
-                    matchedEntireInput shouldBe false
-                    matchedInput shouldBe null
-                    restOfInput shouldBe "${chars[2]}"
-                }
+    "CharRange rule does not match character below of range" {
+        checkAll(Arb.set(Arb.char(), 3..3)) { chars ->
+            val (below, lower, upper) = chars.toList().sorted().let { Triple(it[0], it[1], it[2]) }
+            Parser(object : AbstractGrammar<Int>() {
+                override fun root() = charRange(lower, upper)
+            }).run("$below").apply {
+                matched shouldBe false
+                matchedEntireInput shouldBe false
+                matchedInput shouldBe null
+                restOfInput shouldBe "$below"
+            }
+        }
+    }
+    "CharRange rule does not match character above of range" {
+        checkAll(Arb.set(Arb.char(), 3..3)) { chars ->
+            val (lower, upper, above) = chars.toList().sorted().let { Triple(it[0], it[1], it[2]) }
+            Parser(object : AbstractGrammar<Int>() {
+                override fun root() = charRange(lower, upper)
+            }).run("$above").apply {
+                matched shouldBe false
+                matchedEntireInput shouldBe false
+                matchedInput shouldBe null
+                restOfInput shouldBe "$above"
             }
         }
     }
