478ad71d03b40f01a655473559d34ba1397d83c6
Marco Perazzo <marco.perazzo85@gmail.com>
2021-02-13 01:02:11 +0100

    Distinct on mapped strings

--- src/main/kotlin/com/mpe85/grampa/rule/impl/TrieRule.kt
+++ src/main/kotlin/com/mpe85/grampa/rule/impl/TrieRule.kt
@@ -25,7 +25,7 @@ import kotlin.streams.toList
 public open class TrieRule<T> constructor(private val strings: Collection<String>) : AbstractRule<T>() {
 
     private val trie = CharsTrieBuilder().run {
-        strings.distinct().forEach { add(map(it), 0) }
+        strings.map(::map).distinct().forEach { add(it, 0) }
         build(FAST)
     }
 
