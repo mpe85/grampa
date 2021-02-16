6ca8c7ac0e19eb10b366fc6214d1748e5e918abf
Marco Perazzo <marco.perazzo85@gmail.com>
2021-02-16 23:31:37 +0100

    OptionalRuleTests, ZeroOrMoreRuleTests, OneOrMoreRuleTests

---
+++ src/test/kotlin/com/mpe85/grampa/grammar/OptionalRuleTests.kt
@@ -0,0 +1,37 @@
+package com.mpe85.grampa.grammar
+
+import com.ibm.icu.lang.UCharacter.toString
+import com.mpe85.grampa.lowerCaseCodePoints
+import com.mpe85.grampa.parser.Parser
+import com.mpe85.grampa.rule.impl.toRule
+import com.mpe85.grampa.upperCaseCodePoints
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.property.checkAll
+
+class OptionalRuleTests : StringSpec({
+    "Optional rule matches if child rule matches" {
+        checkAll(lowerCaseCodePoints()) { cp ->
+            Parser(object : AbstractGrammar<Unit>() {
+                override fun root() = optional(cp.value.toRule())
+            }).run(toString(cp.value)).apply {
+                matched shouldBe true
+                matchedEntireInput shouldBe true
+                matchedInput shouldBe toString(cp.value)
+                restOfInput shouldBe ""
+            }
+        }
+    }
+    "Optional rule matches if child rule does not match" {
+        checkAll(lowerCaseCodePoints(), upperCaseCodePoints()) { lower, upper ->
+            Parser(object : AbstractGrammar<Unit>() {
+                override fun root() = optional(lower.value.toRule())
+            }).run(toString(upper.value)).apply {
+                matched shouldBe true
+                matchedEntireInput shouldBe false
+                matchedInput shouldBe ""
+                restOfInput shouldBe toString(upper.value)
+            }
+        }
+    }
+})
---
+++ src/test/kotlin/com/mpe85/grampa/grammar/ZeroOrMoreRuleTests.kt
@@ -0,0 +1,56 @@
+package com.mpe85.grampa.grammar
+
+import com.ibm.icu.lang.UCharacter.toString
+import com.mpe85.grampa.lowerCaseCodePoints
+import com.mpe85.grampa.parser.Parser
+import com.mpe85.grampa.rule.impl.toRule
+import com.mpe85.grampa.upperCaseCodePoints
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.property.Arb
+import io.kotest.property.arbitrary.positiveInts
+import io.kotest.property.checkAll
+
+class ZeroOrMoreRuleTests : StringSpec({
+    "ZeroOrMore rule matches if child rule matches once" {
+        checkAll(lowerCaseCodePoints()) { cp ->
+            Parser(object : AbstractGrammar<Unit>() {
+                override fun root() = zeroOrMore(cp.value.toRule())
+            }).run(toString(cp.value)).apply {
+                matched shouldBe true
+                matchedEntireInput shouldBe true
+                matchedInput shouldBe toString(cp.value)
+                restOfInput shouldBe ""
+            }
+        }
+    }
+    "ZeroOrMore rule matches if child rule matches multiple times" {
+        checkAll(lowerCaseCodePoints(), Arb.positiveInts(10)) { cp, n ->
+            Parser(object : AbstractGrammar<Unit>() {
+                override fun root() = zeroOrMore(cp.value.toRule())
+            }).apply {
+                val repeated = toString(cp.value).repeat(n)
+                run(repeated).apply {
+                    matched shouldBe true
+                    matchedEntireInput shouldBe true
+                    matchedInput shouldBe repeated
+                    restOfInput shouldBe ""
+                }
+            }
+        }
+    }
+    "ZeroOrMore rule matches if child rule does not match" {
+        checkAll(lowerCaseCodePoints(), upperCaseCodePoints()) { lower, upper ->
+            Parser(object : AbstractGrammar<Unit>() {
+                override fun root() = zeroOrMore(lower.value.toRule())
+            }).apply {
+                run(toString(upper.value)).apply {
+                    matched shouldBe true
+                    matchedEntireInput shouldBe false
+                    matchedInput shouldBe ""
+                    restOfInput shouldBe toString(upper.value)
+                }
+            }
+        }
+    }
+})
