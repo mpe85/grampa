b8a4f7f8101acd82a2989f73bd0ce351f28e939d
Marco Perazzo <marco.perazzo85@gmail.com>
2021-02-22 23:35:28 +0100

    PeekRuleTests, PokeRuleTests, PopRuleTests

---
+++ src/test/kotlin/com/mpe85/grampa/grammar/PopRuleTests.kt
@@ -0,0 +1,74 @@
+package com.mpe85.grampa.grammar
+
+import com.mpe85.grampa.parser.Parser
+import com.mpe85.grampa.rule.impl.plus
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.property.checkAll
+
+class PopRuleTests : StringSpec({
+    "Pop rule pops value from parser stack" {
+        checkAll<Int> { int ->
+            Parser(object : AbstractGrammar<Int>() {
+                override fun root() = push(int) + pop()
+            }).run("").apply {
+                matched shouldBe true
+                matchedEntireInput shouldBe true
+                matchedInput shouldBe ""
+                restOfInput shouldBe ""
+                stackTop shouldBe null
+                stack.size shouldBe 0
+            }
+        }
+    }
+    "Pop(down) rule pops value from parser stack at given down position" {
+        checkAll<Int, Int> { int1, int2 ->
+            Parser(object : AbstractGrammar<Int>() {
+                override fun root() = push(int1) + push(int2) + pop(1)
+            }).run("").apply {
+                matched shouldBe true
+                matchedEntireInput shouldBe true
+                matchedInput shouldBe ""
+                restOfInput shouldBe ""
+                stackTop shouldBe int2
+                stack.size shouldBe 1
+            }
+        }
+    }
+    "Pop(context) rule pops value from parser stack" {
+        checkAll<Int> { int ->
+            Parser(object : AbstractGrammar<Int>() {
+                override fun root() = push(int) + command { pop(it) shouldBe int }
+            }).run("").apply {
+                matched shouldBe true
+                matchedEntireInput shouldBe true
+                matchedInput shouldBe ""
+                restOfInput shouldBe ""
+                stackTop shouldBe null
+                stack.size shouldBe 0
+            }
+        }
+    }
+    "Pop(down,context) rule pops value from parser stack at given down position" {
+        checkAll<Int, Int> { int1, int2 ->
+            Parser(object : AbstractGrammar<Int>() {
+                override fun root() = push(int1) + push(int2) + command { pop(1, it) shouldBe int1 }
+            }).run("").apply {
+                matched shouldBe true
+                matchedEntireInput shouldBe true
+                matchedInput shouldBe ""
+                restOfInput shouldBe ""
+                stackTop shouldBe int2
+                stack.size shouldBe 1
+            }
+        }
+    }
+    "Pop(value) throws NoSuchElementException when stack is empty" {
+        Parser(object : AbstractGrammar<Unit>() {
+            override fun root() = pop()
+        }).apply {
+            shouldThrow<NoSuchElementException> { run("") }
+        }
+    }
+})
