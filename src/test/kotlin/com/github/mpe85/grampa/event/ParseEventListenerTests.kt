8f01d816776e457a6a8eb00105dac12a8d551ed6
Marco Perazzo <marco.perazzo85@gmail.com>
2021-03-01 23:49:35 +0100

    Test names

--- src/test/kotlin/com/github/mpe85/grampa/event/ParseEventListenerTests.kt
+++ src/test/kotlin/com/github/mpe85/grampa/event/ParseEventListenerTests.kt
@@ -10,7 +10,7 @@ import org.greenrobot.eventbus.Subscribe
 import org.greenrobot.eventbus.SubscriberExceptionEvent
 
 class ParseEventListenerTests : StringSpec({
-    "Listen to event" {
+    "ParseEventListener is called when event is posted" {
         val listener = object : ParseEventListener<String>() {
             var called = false
             override fun beforeParse(event: PreParseEvent<String>) {
@@ -25,7 +25,7 @@ class ParseEventListenerTests : StringSpec({
 
         listener.called shouldBe true
     }
-    "Handle subscriber exception" {
+    "SubscriberExceptionEvent is posted when handler of ParseEventListener throws exception" {
         val ex = RuntimeException("failure")
 
         val listener = object : ParseEventListener<String>() {
