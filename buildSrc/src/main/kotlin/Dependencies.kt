ee78760a01085c910a6e78332ba901e08ca0b00a
Marco Perazzo <marco.perazzo85@gmail.com>
2021-07-15 09:29:10 +0200

    Bumped byte-buddy

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,5 +1,5 @@
 object Versions {
-    const val byteBuddy = "1.11.6"
+    const val byteBuddy = "1.11.7"
     const val detekt = "1.17.1"
     const val dokka = "1.5.0"
     const val eventBus = "3.2.0"
