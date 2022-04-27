245f14cf24c21af29f55959ea0a60176bc8abd46
Marco Perazzo <marco.perazzo85@gmail.com>
2022-04-27 09:34:13 +0200

    Fixed test

--- src/test/kotlin/com/github/mpe85/grampa/grammar/TestNotRuleTests.kt
+++ src/test/kotlin/com/github/mpe85/grampa/grammar/TestNotRuleTests.kt
@@ -1,18 +1,19 @@
 package com.github.mpe85.grampa.grammar
 
 import com.github.mpe85.grampa.legalCodePoints
-import com.github.mpe85.grampa.lowerCaseCodePoints
 import com.github.mpe85.grampa.parser.Parser
-import com.github.mpe85.grampa.upperCaseCodePoints
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.property.Arb
+import io.kotest.property.arbitrary.Codepoint
+import io.kotest.property.arbitrary.arabic
+import io.kotest.property.arbitrary.cyrillic
 import io.kotest.property.arbitrary.string
 import io.kotest.property.checkAll
 
 class TestNotRuleTests : StringSpec({
     "TestNot rule matches failing rule" {
-        checkAll(Arb.string(2..10, lowerCaseCodePoints()), Arb.string(2..10, upperCaseCodePoints())) { lower, upper ->
+        checkAll(Arb.string(2..10, Codepoint.arabic()), Arb.string(2..10, Codepoint.cyrillic())) { lower, upper ->
             Parser(object : AbstractGrammar<Unit>() {
                 override fun start() = testNot(lower.toRule()) + upper.toRule()
             }).run(upper).apply {
