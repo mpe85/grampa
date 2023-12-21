3c8fb98f1e3d1c51dab133365215615f830cc850
Marco Perazzo <marco.perazzo85@gmail.com>
2023-12-21 23:08:02 +0100

    Bumped dependencies

--- examples/json/build.gradle.kts
+++ examples/json/build.gradle.kts
@@ -1,7 +1,7 @@
 group = "com.github.mpe85"
 
 plugins {
-    kotlin("jvm") version "1.9.21"
+    kotlin("jvm") version "1.9.22"
     application
 }
 
@@ -10,7 +10,7 @@ repositories {
 }
 
 dependencies {
-    implementation(kotlin("stdlib-jdk8", "1.9.21"))
+    implementation(kotlin("stdlib-jdk8", "1.9.22"))
     implementation("com.github.mpe85:grampa:1.3.0")
 }
 
