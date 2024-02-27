ea4e9e2e021a59b058ca99e0b782756b3f466338
Marco Perazzo <marco.perazzo85@gmail.com>
2024-02-28 00:36:24 +0100

    Get rid of deprecated method

--- src/main/kotlin/com/github/mpe85/grampa/stack/LinkedListStack.kt
+++ src/main/kotlin/com/github/mpe85/grampa/stack/LinkedListStack.kt
@@ -25,11 +25,11 @@ internal open class LinkedListStack<E> : LinkedList<E>, Stack<E> {
 
     override fun push(down: Int, element: E): Unit = add(down, element)
     override fun pop(down: Int): E = removeAt(down)
-    override fun peek(): E = first
+    override fun peek(): E = first()
     override fun peek(down: Int): E = get(down)
     override fun poke(element: E): E = set(0, element)
     override fun poke(down: Int, element: E): E = set(down, element)
-    override fun dup(): Unit = push(first)
+    override fun dup(): Unit = push(peek())
     override fun swap(): Unit = swap(this, 0, 1)
     override fun copy(): LinkedListStack<E> = LinkedListStack(this)
 
