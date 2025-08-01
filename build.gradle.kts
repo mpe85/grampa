dbf1c5da79131f0a3de1516abc1c3ea0f05eaaac
Marco Perazzo <marco.perazzo85@gmail.com>
2025-08-01 18:07:13 +0200

    Migrate publishing to maven central portal

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 import com.github.benmanes.gradle.versions.updates.gradle.GradleReleaseChannel.CURRENT
+import com.vanniktech.maven.publish.JavadocJar
+import com.vanniktech.maven.publish.KotlinJvm
 import java.util.Locale
-import org.gradle.api.plugins.BasePlugin.BUILD_GROUP
 import org.jetbrains.dokka.gradle.engine.parameters.VisibilityModifier
 
 group = "com.github.mpe85"
@@ -17,8 +18,7 @@ plugins {
     id("org.jetbrains.dokka") version "2.0.0"
     id("org.jetbrains.kotlinx.kover") version "0.9.1"
     id("com.ncorti.ktfmt.gradle") version "0.23.0"
-    id("org.gradle.maven-publish")
-    id("org.gradle.signing")
+    id("com.vanniktech.maven.publish") version "0.34.0"
     id("com.github.ben-manes.versions") version "0.52.0"
 }
 
@@ -71,26 +71,6 @@ additionalTestToolchains.forEach {
     }
 }
 
-val javadocJar by
-    tasks.registering(Jar::class) {
-        group = BUILD_GROUP
-        dependsOn(tasks.dokkaGenerate)
-        archiveClassifier.set("javadoc")
-        from(tasks.dokkaGeneratePublicationHtml.get().outputDirectory)
-    }
-val sourcesJar by
-    tasks.registering(Jar::class) {
-        group = BUILD_GROUP
-        dependsOn("classes")
-        archiveClassifier.set("sources")
-        from(sourceSets.main.get().allSource)
-    }
-
-artifacts {
-    archives(javadocJar)
-    archives(sourcesJar)
-}
-
 tasks {
     jar {
         manifest {
@@ -114,59 +94,40 @@ tasks {
     }
 }
 
-publishing {
-    repositories {
-        maven {
-            val releasesRepoUrl = "https://s01.oss.sonatype.org/content/repositories/releases/"
-            val snapshotsRepoUrl = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
-            url =
-                uri(
-                    if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl
-                    else releasesRepoUrl
-                )
-            credentials {
-                username = "ossrhUsername".property()
-                password = "ossrhPassword".property()
-            }
-        }
-    }
-    publications {
-        register<MavenPublication>("mavenJava") {
-            from(components["java"])
-            artifact(sourcesJar) { classifier = "sources" }
-            artifact(javadocJar) { classifier = "javadoc" }
-            groupId = "$group"
-            artifactId = project.name
-            version = "${project.version}"
+mavenPublishing {
+    publishToMavenCentral()
+    signAllPublications()
 
-            pom {
-                name.set("Grampa")
-                description.set("Grampa")
-                url.set(gitUrl)
-                licenses {
-                    license {
-                        name.set("MIT")
-                        url.set("$gitUrl/blob/main/LICENSE")
-                    }
-                }
-                developers {
-                    developer {
-                        id.set("mpe85")
-                        name.set("Marco Perazzo")
-                        email.set("marco.perazzo85@gmail.com")
-                    }
-                }
-                scm {
-                    connection.set(gitScmUrl)
-                    developerConnection.set(gitScmUrl)
-                    url.set(gitUrl)
-                }
+    coordinates("$group", project.name, "$version")
+
+    pom {
+        name.set("Grampa")
+        description.set("Grampa")
+        url.set(gitUrl)
+        licenses {
+            license {
+                name.set("MIT")
+                url.set("$gitUrl/blob/main/LICENSE")
             }
         }
+        developers {
+            developer {
+                id.set("mpe85")
+                name.set("Marco Perazzo")
+                email.set("marco.perazzo85@gmail.com")
+            }
+        }
+        scm {
+            connection.set(gitScmUrl)
+            developerConnection.set(gitScmUrl)
+            url.set(gitUrl)
+        }
     }
+
+    configure(
+        KotlinJvm(javadocJar = JavadocJar.Dokka("dokkaGeneratePublicationHtml"), sourcesJar = true)
+    )
 }
-
-signing { sign(publishing.publications["mavenJava"]) }
 
 fun String.isNonStable(): Boolean {
     val stableKeyword =
@@ -175,5 +136,3 @@ fun String.isNonStable(): Boolean {
     val isStable = stableKeyword || regex.matches(this)
     return isStable.not()
 }
-
-fun String.property() = if (hasProperty(this)) property(this)?.toString() else null
